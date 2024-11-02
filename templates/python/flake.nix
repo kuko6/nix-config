652c25d24cdb3b45c8de87888043819cb0e7de39
Jakub Povinec <jakub.povinec@gmail.com>
2024-11-02 21:25:58 +0100

    updated python template

--- templates/python/flake.nix
+++ templates/python/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Simple Python development environment";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";  
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";  
   outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
