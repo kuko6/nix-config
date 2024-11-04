2753454297d9770114925fc91319fe1909c620f5
Jakub Povinec <jakub.povinec@gmail.com>
2024-11-04 22:24:27 +0100

    added gnome keybindings

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -3,13 +3,13 @@
 {
   home.username = "kuko";
   home.homeDirectory = "/home/kuko";
-  
+
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
-  
+
   # Packages installed to the user profile 
   # packages doesnt need to be here if they are included in programs
   home.packages = with pkgs; [
@@ -21,21 +21,28 @@
     gnomeExtensions.dash-to-dock
   ];
 
-  programs.git = {
-  	enable = true;
-  	userName = "Jakub Povinec";
-  	userEmail = "jakub.povinec@gmail.com";
-
-  	extraConfig = {
-  		init = {
-  			defaultBranch = "main";
-  		};
+  dconf.settings = {
+    "org/gnome/desktop/wm/keybindings" = {
+      search = [ "<Super>S" ];
+      close = [ "<Super>Q" ];
+      switch-to-workspace-left = [ "<Control>Left" ];
+      switch-to-workspace-right = [ "<Control>Right" ];
+      move-to-workspace-left = [ "<Control><Shift>Left" ];
+      move-to-workspace-right = [ "<Control><Shift>Right" ];
     };
-	};
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Jakub Povinec";
+    userEmail = "jakub.povinec@gmail.com";
+
+    extraConfig = { init = { defaultBranch = "main"; }; };
+  };
 
   programs = {
-    zsh = import ./zsh.nix {inherit config pkgs;};
-    helix = import ./helix.nix {inherit config pkgs;};
+    zsh = import ./zsh.nix { inherit config pkgs; };
+    helix = import ./helix.nix { inherit config pkgs; };
   };
 
   programs.home-manager.enable = true;
