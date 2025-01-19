c71383065d0188320d84e67099d6d8cbfae065a3
Jakub Povinec <jakub.povinec@gmail.com>
2025-01-19 16:06:42 +0100

    fixed background scripts

--- bin/randwall.sh
+++ bin/randwall.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+pkill -x swaybg
 wall=$(find ~/Pictures/Wallpapers -type f -not -name ".DS_Store" | shuf -n 1)
 echo $wall
 swaybg -i "$wall" -m fill &
--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+pkill -x swaybg
 exec swaybg -i ~/Images/mitski.jpg -m fill
